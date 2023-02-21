2b5da9b45a21a21c8da18f02ff9d5c3b11046df0
neetaga1 <125275091+neetaga1@users.noreply.github.com>
2023-02-21 17:29:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,6 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-from urllib.error import URLerror
-
-
-
 
 streamlit.title('My Mom''s New Healthy Diner')
 streamlit.header ('Breakfast Favorites')
